1aaf5614f660025ff0e1d1f2e88c3088cd5523a0
Upain pathania <272876398+Upainpathania@users.noreply.github.com>
2026-04-14 10:18:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -256,34 +256,9 @@ if file:
             st.write("Precision:", precision_score(y_test, y_pred, average='weighted'))
             st.write("Recall:", recall_score(y_test, y_pred, average='weighted'))
             st.write("F1:", f1_score(y_test, y_pred, average='weighted'))
+            st.subheader("Confusion Matrix")
             cm = confusion_matrix(y_test, y_pred)
-
-            fig, ax = plt.subplots()
-            
-            cax = ax.matshow(cm, cmap='Blues')
-            plt.title("Confusion Matrix")
-            plt.colorbar(cax)
-            
-            ax.set_xlabel('Predicted')
-            ax.set_ylabel('Actual')
-            
-            # Set labels dynamically
-            labels = np.unique(y_test)
-            
-            ax.set_xticks(np.arange(len(labels)))
-            ax.set_yticks(np.arange(len(labels)))
-            
-            ax.set_xticklabels(labels)
-            ax.set_yticklabels(labels)
-            
-            # Add numbers inside boxes
-            for i in range(len(labels)):
-                for j in range(len(labels)):
-                    ax.text(j, i, cm[i, j],
-                            ha="center", va="center",
-                            color="black")
-            
-            st.pyplot(fig)
+            st.text(cm)
                  
 
         else:
